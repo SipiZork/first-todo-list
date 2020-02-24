c0e8c2d662497494fdded25e1d508e55a324e471
Mark <sipizork@gmail.com>
2020-02-25 00:15:01 +0100

    fixed

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,6 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({ todos: todos });
     this.ref = base.syncState("todos", {
       context: this,
       state: "todos" });
