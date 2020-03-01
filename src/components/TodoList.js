9889787f93943d07cc3a630c02a0b806e8fc5000
Mark <sipizork@gmail.com>
2020-03-02 00:42:39 +0100

    mobile view

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,12 +3,17 @@ import '../css/TodoList.css';
 
 class TodoList extends Component {
 
+  openTodoList = id => {
+    this.props.openTodoList(id);
+    this.props.toggleMenu();
+  }
+
   createTodo = () => {
     const { details, id } = this.props;
     return (
       <Fragment>
         <div className="todo-list-container">
-          <div className="list-item" onClick={() => this.props.openTodoList(id)}>
+          <div className="list-item" onClick={() => this.openTodoList(id)}>
             {details.name}
               </div>
                 <div className="list-remove" onClick={() => this.props.removeListFromTodos(id)}>
--- src/components/TodoMenu.js
+++ src/components/TodoMenu.js
@@ -4,7 +4,8 @@ import TodoList from './TodoList.js'
 class TodoMenu extends Component {
 
   state = {
-    value: ""
+    value: "",
+    menu: false
   }
 
   handleChange = e => {
@@ -21,40 +22,61 @@ class TodoMenu extends Component {
     this.setState({ value: "" });
   }
 
+  toggleMenu = () => {
+    this.setState({ menu: !this.state.menu });
+  }
+
+  checked = () => {
+    if(this.state.menu === true) {
+      return "checked";
+    } else {
+      return "";
+    }
+  }
+
+  checkOnChange = () => {
+
+  }
+
   render() {
     return (
       <Fragment>
-        <div className="menu">
-          <div className="user">
-            <div className="logout" onClick={this.props.logout}>
-              X
+        <div className="open-menu">
+          <input type="checkbox" className="toggler" onClick={this.toggleMenu} checked={this.state.menu === true ? "checked" : ""} onChange={this.checkOnChange}/>
+          <div className="hamburger"><div></div></div>
+          <div className="menu">
+            <div className="user">
+              <div className="logout" onClick={this.props.logout}>
+                X
+              </div>
+              <div className="user-email">
+                {this.props.user.email}
+              </div>
             </div>
-            <div className="user-email">
-              {this.props.user.email}
+            <div className="todos">
+              <p>Listák</p>
+              {Object.keys(this.props.todos).map(key => (
+                <TodoList
+                  key={key}
+                  id={key}
+                  details={this.props.todos[key]}
+                  openTodoList={this.props.openTodoList}
+                  removeListFromTodos={this.props.removeListFromTodos}
+                  user={this.props.user}
+                  toggleMenu={this.toggleMenu}
+                />
+              ))}
+              <form onSubmit={(e) => this.createTodoList(e)}>
+                <input
+                  type="text"
+                  name="addTodo"
+                  className="add-todo"
+                  placeholder="Lista hozzáadása"
+                  autoComplete="off"
+                  value={this.state.value}
+                  onChange={this.handleChange}/>
+              </form>
             </div>
-          </div>
-          <div className="todos">
-            <p>Listák</p>
-            {Object.keys(this.props.todos).map(key => (
-              <TodoList
-                key={key}
-                id={key}
-                details={this.props.todos[key]}
-                openTodoList={this.props.openTodoList}
-                removeListFromTodos={this.props.removeListFromTodos}
-                user={this.props.user}
-              />
-            ))}
-            <form onSubmit={(e) => this.createTodoList(e)}>
-              <input
-                type="text"
-                name="addTodo"
-                className="add-todo"
-                placeholder="Lista hozzáadása"
-                autoComplete="off"
-                value={this.state.value}
-                onChange={this.handleChange}/>
-            </form>
           </div>
         </div>
       </Fragment>
