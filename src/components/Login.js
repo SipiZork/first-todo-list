fece94fc0dca91e8e2ee76fd7714a522001486d7
Mark <sipizork@gmail.com>
2020-03-20 20:16:10 +0100

    hotfix

--- src/components/Login.js
+++ src/components/Login.js
@@ -206,10 +206,6 @@ class Login extends Component {
   render() {
     return (
       <Fragment>
-        <svg>
-          <path className="arrow"></path>
-          <path className="line"></path>
-        </svg>
         {this.state.register === false ? this.renderLogIn() : this.renderRegister()}
       </Fragment>
     )
